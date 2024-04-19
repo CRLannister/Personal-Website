442e7f3d0e8e23e157d9835088d769bb6777687a
Ashish Agarwal <29420145+CRLannister@users.noreply.github.com>
2024-04-19 09:47:58 -0400

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -330,20 +330,6 @@ const blogSection = {
   subtitle:
     "I try to write and sometime publish some blogs !",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
-  blogs: [
-    {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    }
-  ],
   display: true // Set false to hide this section, defaults to true
 };
 
